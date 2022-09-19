a5a1eb89c99c1e2b82e24f0ad5b9ef82b680ca02
haykgrigoryan77 <haykgrigoryan077@mail.ru>
2022-09-19 20:26:28 +0400

    SECOND HOMEWORK

---
+++ homeWork2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int* bubbleSort(int arr[], int indexArr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(indexArr[j], indexArr[j + 1]);
+    
+    return indexArr;
+}
+
+
+
+int main()
+{
+    int size = 5;
+    int arr[size] = {5, 4, 3, 2, 1};
+    int indexes[size];
+    for (int i = 0; i < size; i++) {
+        indexes[i] = i;
+    }
+    
+    int *array = bubbleSort(arr, indexes, size);
+    for (int i = 0; i < size; i++) {
+        cout << indexes[i] << endl;
+    }
+
+    return 0;
+}
