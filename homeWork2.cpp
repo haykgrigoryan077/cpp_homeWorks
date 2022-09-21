7753f71d49be078b952b687db4c58f2155dba0af
haykgrigoryan77 <haykgrigoryan077@mail.ru>
2022-09-21 22:43:48 +0400

    FORGOT ONE LINE< NOW FINAL

--- homeWork2.cpp
+++ homeWork2.cpp
@@ -5,10 +5,12 @@ using namespace std;
 int* bubbleSort(int arr[], int indexArr[], int n)
 {
     int i, j;
-    for (i = 0; i < n - 1; i++)
-        for (j = 0; j < n - i - 1; j++)
-            if (arr[j] > arr[j + 1])
+    for (i = 0; i < n - 1; i++){
+        for (j = 0; j < n - i - 1; j++){
+            if (arr[j] > arr[j + 1]){
                 swap(indexArr[j], indexArr[j + 1]);
+                swap(arr[j], arr[j + 1]);
+            }}}
     
     return indexArr;
 }
